5cefd507bd47b475b22c6269699493b45cecd430
sukovanej <Milansuk@email.cz>
2018-01-17 00:08:47 +0100

    symbol unittests

--- tests/test_symbol.py
+++ tests/test_symbol.py
@@ -8,7 +8,16 @@ class TestGetCurrencyFromSymbol(unittest.TestCase):
 
     def test_get_currency_from_symbol(self):
         config = ConfigParser()
-        config.set("s", "s1", "S1")
+        config.add_section('symbols')
+        config["symbols"]  = {
+            "s1": "S1",
+            "s2": "S2"
+        }
 
         input1 = "s1"
+        input2 = "s2"
+        input3 = "old"
+
         self.assertEqual(get_currency_from_symbol(input1, config), "S1")
+        self.assertEqual(get_currency_from_symbol(input2, config), "S2")
+        self.assertEqual(get_currency_from_symbol(input3, config), "old")
