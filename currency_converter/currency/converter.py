336b1e4abb3b1c9ad8aa0b24390dadd6672eb44b
sukovanej <suk@eurosat.cz>
2018-01-20 14:16:00 +0100

    compose

--- currency_converter/currency/converter.py
+++ currency_converter/currency/converter.py
@@ -4,9 +4,9 @@ import redis
 class Converter(object):
 
     def __init__(self, config):
-        self.pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
+        self.pool = redis.Redis(host='localhost', port=6379, db=0)
         self.precision = config.getint('data', 'precision')
 
     def convert_from_to(self, from_currency, to_currency):
-        return round(self.pool.get(from_currency) /
-                     self.pool.get(to_currency), self.precision)
+        return round(self.pool.get(from_currency) / self.pool.get(to_currency),
+                     self.precision)
